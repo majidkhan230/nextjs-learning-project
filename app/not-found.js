7e702169d0bbf88b4bb9d3be7591426a0afbbf7a
MajidKh230 <khanmajid230@gmail.com>
2024-10-20 13:36:13 +0500

    some changes made

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='text-3xl flex justify-center items-center w-full h-screen font-bold text-red-500 uppercase'
+    >404 not found</div>
+  )
+}
+
+export default NotFound
